45afb946fc282e84f3791de5623737e22a6c8b3a
AnahitaFB <ana.f.r.2003@gmail.com>
2024-07-04 10:31:49 +0330

    University

---
+++ domain/University.py
@@ -0,0 +1,7 @@
+class University:
+    def __init__(self, UniversityName, CityID, InternationalsAsTA, FallDeadline, WinterDeadline):
+        self.UniversityName = UniversityName;
+        self.CityID = CityID;
+        self.InternationalsAsTA = InternationalsAsTA;
+        self.FallDeadline = FallDeadline;
+        self.WinterDeadline =   WinterDeadline;
