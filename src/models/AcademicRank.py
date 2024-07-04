20cd7eee507cea9891c0166b4d158e4af7960aef
sepehragz82 <sepehr.agz82@gmail.com>
2024-07-04 16:06:33 +0330

    fix models

--- src/models/AcademicRank.py
+++ src/models/AcademicRank.py
@@ -1,5 +1,4 @@
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Text, DateTime
-from sqlalchemy.orm import relationship
 from src.database.session import Base
 
 
@@ -8,5 +7,3 @@ class academic_rank(Base):
 
     AcademicRankID = Column(Integer, primary_key=True, index=True)
     AcademicRankTitle = Column(String, index=True)
-
-    author_id = relationship("User", back_populates="notes")
