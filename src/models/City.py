3cf7a0d9bfd34fe6c7fcd1218135af884804c0c8
Behnam Salehi <behnam.dev@outlook.com>
2024-07-04 16:56:39 +0330

    Update City.py

--- src/models/City.py
+++ src/models/City.py
@@ -4,10 +4,10 @@ from sqlalchemy.orm import relationship
 from src.database.session import Base
 
 
-class city(Base):
+class City(Base):
     __tablename__ = "city"
 
-    CityID = Column(Integer, primary_key=True)
-    CityName = Column(String)
+    cityID = Column(Integer, primary_key=True)
+    cityName = Column(String)
 
-    CountryID = relationship("city", back_populates="country")
+    countryID = relationship("city", back_populates="country")
