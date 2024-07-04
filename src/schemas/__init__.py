9a7923fd404c4d9cc38970f3f129c5ee9d23cb20
Behnam Salehi <behnam.dev@outlook.com>
2024-07-04 21:39:52 +0330

    Update schemas __init__

--- src/schemas/__init__.py
+++ src/schemas/__init__.py
@@ -1,14 +1,18 @@
-from .academic_rank import *
-from .city import *
-from .country import *
-from .department import *
-from .fund import *
-from .item import *
-from .position import *
-from .position_type import *
-from .prof_interest import *
-from .professor import *
-from .research_interest import *
-from .token import *
-from .university import *
-from .user import *
+from .academic_rank import AcademicRank, AcademicRankCreate, AcademicRankUpdate
+from .city import City, CityCreate, CityUpdate
+from .country import Country, CountryCreate, CountryUpdate
+from .department import Department, DepartmentCreate, DepartmentUpdate
+from .fund import Fund, FundCreate, FundUpdate
+from .item import Item, ItemCreate, ItemUpdate
+from .position import Position, PositionCreate, PositionUpdate
+from .position_type import PositionType, PositionTypeCreate, PositionTypeUpdate
+from .prof_interest import ProfInterest, ProfInterestCreate, ProfInterestUpdate
+from .professor import Professor, ProfessorCreate, ProfessorUpdate
+from .research_interest import (
+    ResearchInterest,
+    ResearchInterestCreate,
+    ResearchInterestUpdate,
+)
+from .token import Token, TokenData
+from .university import University, UniversityCreate, UniversityUpdate
+from .user import User, UserCreate, UserUpdate
