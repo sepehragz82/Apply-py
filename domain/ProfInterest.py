2d7cac7fb12354fc3b660bc339778390d47391e0
Pariya Palizban <Pariya_pl@yahoo.com>
2024-07-04 11:30:53 +0330

    Updates on IDs

--- domain/ProfInterest.py
+++ domain/ProfInterest.py
@@ -1,4 +1,5 @@
 class ProfInterest:
-    def __init__(self, ProfessorID, ResearchInterestID):
+    def __init__(self, ProfInterestID, ProfessorID, ResearchInterestID):
+        self.ProfInterestID = ProfInterestID
         self.ProfessorID = ProfessorID
         self.ResearchInterestID = ResearchInterestID
