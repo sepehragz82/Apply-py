6e5ed4faca88015fed1252b492d24420b8e5ec39
sepehragz82 <sepehr.agz82@gmail.com>
2024-07-04 17:46:01 +0330

    aaaaaaaaaassqsqsqsqsq

--- src/schemas/department.py
+++ src/schemas/department.py
@@ -1,14 +1,16 @@
-from pydantic import BaseModel
+from typing import Union
 
+from pydantic import BaseModel
 
 class DepartmentCreate(BaseModel):
     departmentName: str
 
-
 class DepartmentUpdate(BaseModel):
     departmentName: str
-
 
 class Department(BaseModel):
     departmentID: int
     departmentName: str
+
+class Config:
+    orm_mode = True
