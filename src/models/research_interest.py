5222f3c31f80d0b6a511afa4b9bd09a81ab9f48a
Behnam Salehi <behnam.dev@outlook.com>
2024-07-04 16:53:12 +0330

    Update ResearchInterest

--- src/models/research_interest.py
+++ src/models/research_interest.py
@@ -3,9 +3,9 @@ from sqlalchemy import Boolean, Column, DateTime, ForeignKey, Integer, String, T
 from src.database.session import Base
 
 
-class research_interest(Base):
+class ResearchInterest(Base):
     __tablename__ = "research_interest"
 
-    ResearchInterestID = Column(Integer, primary_key=True)
-    ResearchInterestName = Column(String)
-    Description = Column(String)
+    researchInterestID = Column(Integer, primary_key=True)
+    researchInterestName = Column(String)
+    description = Column(String)
