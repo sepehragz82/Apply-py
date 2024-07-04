1517852b88eb7c92ab56c04abd760a9279f79439
NimaGhasemzadeh1 <nima.ghasemzadeh.00@gmail.com>
2024-07-04 12:45:52 +0330

    Nima

--- src/schemas/item.py
+++ src/schemas/item.py
@@ -7,3 +7,21 @@ class Item(BaseModel):
     name: str
     price: float
     is_offer: Union[bool, None] = None
+
+class Professor(BaseModel):
+    ProfessorID = ProfessorID
+    ProfessorFName = ProfessorFName
+    ProfessorMName = ProfessorMName
+    ProfessorLName = ProfessorLName
+    ProfGender = ProfGender
+    ProfCodeinUni = ProfCodeinUni
+    UniversityID = UniversityID
+    DepartmentID = DepartmentID
+    Email = Email
+    inkedIN =LinkedIN
+    GoogleScholar = GoogleScholar
+    H_Index = H_Index
+    ProfileUniSite = ProfileUniSite
+    EducationDescription = EducationDescription
+    ExtraDescription = ExtraDescription
+    
