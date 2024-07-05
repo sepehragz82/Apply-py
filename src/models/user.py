dce3d8e467573a9e1a00f36a95b7e130707b65e8
Behnam Salehi <behnam.dev@outlook.com>
2024-07-05 06:24:12 +0330

    Update User

--- src/models/user.py
+++ src/models/user.py
@@ -1,4 +1,7 @@
-from sqlalchemy import Column, DateTime, Integer, String
+from datetime import datetime
+
+from sqlalchemy import DateTime
+from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.sql import func
 
 from src.database.session import Base
@@ -7,10 +10,14 @@ from src.database.session import Base
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True)
-    username = Column(String, unique=True)
-    hashed_password = Column(String)
-    name = Column(String, nullable=True)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True)
+    hashed_password: Mapped[str] = mapped_column()
+    name: Mapped[str] = mapped_column(nullable=True)
 
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
-    modified_at = Column(DateTime(timezone=True), server_default=func.now())
+    createdAt: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now()
+    )
+    modifiedAt: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now()
+    )
